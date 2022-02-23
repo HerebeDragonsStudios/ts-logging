58ecc8e8feeac1bf9fedd1a3e7958091894fd3ff
Vítor Nogueira <vitor.nogueira@pdmfc.com>
2022-02-23 17:32:24 +0000

    refs #1 gulp webpack bundle task added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const gulp = require('gulp');
 const {src, dest, parallel, series} = gulp;
 const ts = require('gulp-typescript');
@@ -6,6 +8,10 @@ const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
+
+const named = require('vinyl-named');
+
+const webpack = require('webpack-stream');
 
 const tsOptions = {
     "emitDeclarationOnly": true,
@@ -41,21 +47,47 @@ function exportDefault() {
         .pipe(dest('dist/'));
 }
 
-function exportModulesBundles() {
-    const tsProject = ts.createProject('tsconfig.json', Object.assign({}, tsOptions, {module: "umd"}));
-    return src('src/**/*.ts')
-        .pipe(sourcemaps.init())
-        .pipe(tsProject())
-        .pipe(babel({
-            presets: [['@babel/preset-env', {
-                targets: {"esmodules": true}
-            }]]
+function exportModulesBundles(){
+    const entryFile = "src/index.ts"
+
+    return src(entryFile)
+        .pipe(named())
+        .pipe(webpack({
+            mode: 'development', // can be changed to production to produce minified bundle
+    
+            devtool: 'eval-source-map',
+
+            module: {
+                rules: [
+                    {
+                        test: /\.ts$/,
+                        use: [{
+                            loader: 'ts-loader',
+                            options: {
+                                configFile: 'tsconfig.webpack.json'
+                            }
+                        }],
+                        include:[
+                            path.resolve(__dirname, "src")
+                        ],
+                        exclude: /node_modules/,
+                    }
+                ]
+            },
+        
+            resolve: {
+                extensions: ['.ts', '.js']
+            },
+        
+            output: {
+                filename: 'index.bundle.min.esm.js',
+                path: path.resolve(__dirname, "dist/build"), //output path
+                library: 'logging',
+                libraryTarget: 'umd',
+                umdNamedDefine: true
+            }
         }))
-        .pipe(concat('index.bundle.js'))
-        .pipe(uglify())
-        .pipe(rename({ extname: '.min.esm.js' }))
-        .pipe(sourcemaps.write())
-        .pipe(dest('dist/esm/'));
+        .pipe(dest("./dist/esm"));
 }
 
 function exportDefaultBundles() {
