3f4659d858431cb43d61cf455a2cb372be630844
Tiago <tiago.venceslau@pdmfc.com>
2022-03-04 15:51:22 +0000

    after release preparation

--- lib/logging/utils.d.ts
+++ lib/logging/utils.d.ts
@@ -1,3 +1,4 @@
+export declare function getObjectName(obj: any): string | undefined;
 export declare function stringFormat(string: string, ...args: string[]): string;
 export declare function formatDate(date: Date, patternStr?: string): string;
 export declare function twoDigitPad(num: number): string;
